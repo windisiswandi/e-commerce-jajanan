93a57811a4704603109526ebd7a02aee90f7eddc
windisiswandi <windisiswandi89@gmail.com>
2024-07-17 22:00:55 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./application/views/**/*.php"],
+	theme: {
+		screens: {
+			sm: "640px",
+			md: "768px",
+			lg: "1024px",
+			xl: "1280px",
+			"2xl": "1440px",
+			"3xl": "1600px",
+		},
+		extend: {
+			colors: {
+				primary: "#9c4a29",
+				secondary: "#edebe4",
+				three: "#c9ba86",
+			},
+			animation: {
+				popupAnimation: "popupAnimation 0.3s ease",
+				scrollAnitmation: "scrollAnitmation 0.3s ease",
+			},
+			keyframes: {
+				popupAnimation: {
+					"0%": { transform: "scale(0.5)", opacity: 0 },
+					"100%": { transform: "scale(1)", opacity: 1 },
+				},
+				scrollAnitmation: {
+					"0%": { transform: "translateY(-100%)" },
+					"100%": { transform: "translateY(0)" },
+				},
+			},
+		},
+		container: {
+			center: true,
+		},
+		fontFamily: {
+			"open-sans": ['"Open Sans"'],
+		},
+	},
+	plugins: [],
+};
+
