a435c67ee9df6c25277f9b97eda2caa4ebaef75c
windisiswandi <windisiswandi89@gmail.com>
2024-07-20 06:55:04 +0800

    feat: add footer

--- application/views/components/footer.php
+++ application/views/components/footer.php
@@ -1,4 +1,54 @@
-    
+    <footer class="bg-gray-100">
+        <div class="xl:container py-8 sm:py-16 px-4 sm:px-8">
+            <div class="grid grid-cols-4 gap-4">
+                <div>
+                    <h1 class="font-semibold text-2xl">Toko Jajanan Lombok</h1>
+                    <p class="text-gray-500 text-sm mt-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <div class="space-x-3 text-black font-bold text-xl mt-5">
+                        <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                        <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                    </div>
+                </div>
+                <div>
+                    <h1 class="font-semibold text-base">Pembayaran</h1>
+                    <div class="text-gray-600 text-sm mt-3 grid grid-cols-5 gap-2">
+                        <img src="https://down-id.img.susercontent.com/file/59185c096964e1892e9122ceaae2609d">
+                        <img src="https://down-id.img.susercontent.com/file/9a08d3abab3dd059fff945c72ca372d9">
+                        <img src="https://down-id.img.susercontent.com/file/0cf8caa250763eefc3d79bb1f8c08e73">
+                        <img src="https://down-id.img.susercontent.com/file/id-50009109-8d834dd660b129d1d3c72d22c1cb8867">
+                    </div>
+                    <h1 class="font-semibold text-base mt-6">Pengiriman</h1>
+                    <div class="text-gray-600 text-sm mt-3 grid grid-cols-5 gap-2">
+                        <img src="https://down-id.img.susercontent.com/file/5c07836b22c5689d56cb217c88171785">
+                    </div>
+                </div>
+                <div>
+                    <h1 class="font-semibold text-base">Menu</h1>
+                    <a href="#" class="text-gray-500 mt-3 text-sm block">Tentang Kami</a>
+                    <a href="#" class="text-gray-500 mt-3 text-sm block">Hubungi Kami</a>
+                </div>
+                <div>
+                    <h1 class="font-semibold text-base">Punya Pertanyaan?</h1>
+                    <table class="mt-3 space-y-4">
+                        <tr>
+                            <td class="pr-4"><i class="fa-solid fa-location-dot" class="font-bold"></i></td>
+                            <td><a href="#" class="block text-[12px] text-gray-500">Jl. Melati, Khusus Kota Selong, Kec. Selong, Kabupaten Lombok Timur, Nusa Tenggara Bar. 83618</a></td>
+                        </tr>
+                        <tr>
+                            <td class="pr-4"><i class="fa-brands fa-whatsapp" class="font-bold"></i></td>
+                            <td><a href="#" class="block text-[12px] text-gray-500">+62811213456678</a></td>
+                        </tr>
+                        <tr>
+                            <td class="pr-4"><i class="fa-solid fa-envelope" class="font-bold"></i></td>
+                            <td><a href="#" class="block text-[12px] text-gray-500">tokojajanan.lombok@gmail.com</a></td>
+                        </tr>
+                    </table>
+                    <!-- <iframe class="w-full mt-3" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8976.370175897235!2d116.53855345649421!3d-8.659116886004599!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dcc4f002f414ca9%3A0xf3d932b590e2061f!2sBermis%201%20Selong!5e0!3m2!1sid!2sid!4v1721428911806!5m2!1sid!2sid" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
+                </div>
+            </div>
+        </div>
+    </footer>
     
     <script src="<?= base_url('assets/vendor/slick/slick.min.js'); ?>"></script>
     <script src="<?= base_url('assets/js/carousel-custome.js') ?>"></script>
--- application/views/components/header.php
+++ application/views/components/header.php
@@ -18,3 +18,57 @@
     <link rel="stylesheet" href="<?= base_url('assets/vendor/slick/slick-theme.css'); ?>">
 </head>
 <body>
+    <header class="bg-blue-500">
+        <nav class="px-4 sm:px-8 py-3 xl:container">
+            <div class="justify-between items-center text-white hidden sm:flex">
+                <div class="flex items-center space-x-4">
+                    <div class="flex items-center">
+                        <span class="border-r-2 border-white border-opacity-30 px-2"><i class="fa-solid fa-phone text-sm mr-2"></i> 012343455</span>
+                        <span class="px-2">
+                            Ikuti kami
+                            <a href="#" class="ml-2"><i class="fa-brands fa-facebook"></i></a>
+                            <a href="#" class="mx-2"><i class="fa-brands fa-instagram"></i></a>
+                            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                        </span>
+                    </div>
+                </div>
+                <div class="flex items-center space-x-4">
+                    <a href="#" class="text-white hover:opacity-50">Tentang Kami</a>
+                    <a href="#" class="text-white hover:opacity-50">Kontak</a>
+                    <a href="#" class="text-white flex items-center space-x-1">
+                        <div class="bg-white text-black w-5 h-5 rounded-full flex justify-center items-center text-[12px]">
+                            <i class="fa-solid fa-user"></i>
+                        </div>
+                        <p class="font-bold">tehwinner12</p>
+                    </a>
+                </div>
+            </div>
+            <div class="flex sm:justify-between sm:items-center flex-col sm:flex-row text-white sm:py-5 space-y-2 sm:space-y-0">
+                <a href="#" class="text-lg font-bold text-center sm:text-left sm:text-2xl sm:block">
+                    TOKO JAJANAN LOMBOK
+                </a>
+                <div class="flex-1 flex items-center">
+                    <form action="" class="w-full sm:px-8 mr-5 sm:mr-3">
+                        <div class="bg-white flex rounded overflow-hidden">
+                            <input type="text" placeholder="Cari Produk . . ." class="w-full px-3 sm:p-2 focus:outline-none text-black text-sm sm:text-base">
+                            <button class="text-blue-700 py-2 px-4 focus:outline-none">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                                </svg>
+                            </button>
+                        </div>
+                    </form>
+                    <div class="flex items-center space-x-3 sm:space-x-0">
+                        <a href="#" class="text-white text-lg">
+                            <i class="fa-solid fa-cart-shopping"></i>
+                        </a>
+                        <a href="#" class="text-white text-lg sm:hidden">
+                            <div class="bg-white text-black w-6 h-6 rounded-full flex justify-center items-center text-[12px]">
+                                <i class="fa-solid fa-user"></i>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
