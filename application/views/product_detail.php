c49d013aea870e74aa9e7d292d9c1e5300921d9a
windisiswandi <windisiswandi89@gmail.com>
2024-07-31 22:16:33 +0800

    feat: update detail product page

--- application/views/product_detail.php
+++ application/views/product_detail.php
@@ -1,25 +1,29 @@
-<div class="xl:container px-4 sm:px-8 py-16">
-    <div class="flex justify-center items-start bg-white p-8 rounded-md">
-        <div class="w-1/3">
-            <?php if($product->foto) : ?>
-                <img src="<?= base_url('assets/img/products/'.$product->foto) ?>" class="w-full">
-            <?php else: ?>
-                <img src="<?= base_url('assets/img/no_image.jpg') ?>" class="w-full">
-            <?php endif; ?>
-        </div>
-        <div class="w-2/3 px-16 py-5">
-            <h1 class="font-bold text-2xl"><?= $product->product_name; ?></h1>
-            <h2 class="text-sm text-gray-400"><?= $product->sell; ?> Terjual</h2>
-            <h3 class="text-red-500 text-lg"><?= "Rp". number_format($product->product_price, 0, '.', '.'); ?></h3>
-            <div class="w-44 flex items-center justify-between">
-                <div class="flex p-2 space-x-4 justify-between items-center text-lg">
-                    <button class="text-teal-800">-</button>
-                    <span>1</span>
-                    <button class="text-teal-800">+</button>
-                </div>
-                <div class="font-bold text-lg">Stock: <?= $product->stock; ?></div>
+<div class="xl:container p-4 sm:px-8 md:py-16">
+    <div class="flex justify-center items-start bg-white sm:p-8 rounded-md flex-wrap">
+        <div class="w-full sm:w-[40%] lg:w-1/3">
+            <div>
+                <?php if($product->foto) : ?>
+                    <img src="<?= base_url('assets/img/products/'.$product->foto) ?>" class="w-full max-h-[350px]">
+                <?php else: ?>
+                    <img src="<?= base_url('assets/img/no_image.jpg') ?>" class="w-full max-h-[350px]">
+                <?php endif; ?>
             </div>
-            <button class="text-white px-5 py-2 bg-blue-500 rounded-full">Add to cart</button>
+            <div></div>
+        </div>
+        <div class="w-full sm:w-[60%] lg:w-2/3 p-4 sm:px-8 sm:py-0 lg:px-16">
+            <h1 class="font-bold text-xl sm:text-2xl pb-2"><?= $product->product_name; ?></h1>
+            <h2 class="text-base text-black">Terjual <?= $product->sell; ?></h2>
+            <h2 class="text-red-500 text-xl sm:text-3xl font-semibold py-2"><?= "Rp". number_format($product->product_price, 0, '.', '.'); ?></h2>
+            <div class="mt-5 flex items-center space-x-4">
+                <div class="text-base sm:text-lg py-3">Stock: <span class="font-bold"><?= $product->stock; ?></span></div>
+                <button data-id="<?= $product->id; ?>" onclick="addToCart(this)" class="px-4 py-2 rounded bg-blue-600 text-white"><i class="fa-solid fa-cart-shopping"></i> Add to cart</button>
+            </div>
+            <div class="mt-5">
+                <h2 class="font-bold text-lg">Deskripsi</h2>
+                <p>
+                    <?= $product->description; ?>
+                </p>
+            </div>
         </div>
     </div>
 </div>
